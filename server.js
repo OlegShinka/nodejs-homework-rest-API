c48257b22d872577cfa5e69ce49f6cc62b750209
OlegShinka <volkonxxl@gmail.com>
2023-12-19 20:32:33 +0200

    connect to basedata

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-// const app = require("./app");
+import mongoose from "mongoose";
 import app from "./app.js";
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+// OLd040529p
+const DB_HOST =
+  "mongodb+srv://OlShinka:OLd040529p@cluster0.quxs8kw.mongodb.net/my-contacts?retryWrites=true&w=majority";
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
