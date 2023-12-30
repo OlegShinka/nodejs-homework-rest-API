cdf0da467d978813db5bd0c88c03c7ed6ddec464
OlegShinka <volkonxxl@gmail.com>
2023-12-30 22:51:22 +0200

    /users/current

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -52,4 +52,30 @@ const logUser = async (req, res, next) => {
   });
 };
 
-export { regUser, logUser };
+const currentUser = async (req, res, next) => {
+  const { email, subscription, password } = req.body;
+  const user = await User.findOne({ email }); //перевірка наявності юзера з таким email-ом
+
+  if (!user) {
+    return next(HttpErrors(401, "Email or password is wrong"));
+  }
+
+  const passwordCompare = await bcrypt.compare(password, user.password); // перевірка валідності паролю
+  if (!passwordCompare) {
+    return res.status(401).json({
+      message: "Email or password is wrong",
+    });
+  }
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" }); // генерація токена
+
+  res.json({
+    email: user.email,
+    subscription: user.subscription,
+  });
+};
+
+export { regUser, logUser, currentUser };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,8 +2,11 @@ import express from "express";
 import { validateBody } from "../../decorators/validateBody.js";
 import { userLogSchema, userRegSchema } from "../../schemas/scemas.js";
 import * as authControllers from "../../controllers/auth-controllers.js";
+import authenticate from "../../middlewares/authenticate.js";
 
 const routerAuth = express.Router();
+
+routerAuth.use(authenticate); //не важливо який роут, перевір наявність валідного токена
 
 routerAuth.post(
   "/register",
@@ -12,5 +15,11 @@ routerAuth.post(
 );
 
 routerAuth.post("/login", validateBody(userLogSchema), authControllers.logUser);
+
+routerAuth.get(
+  "/current",
+  validateBody(userLogSchema),
+  authControllers.currentUser
+);
 
 export default routerAuth;
